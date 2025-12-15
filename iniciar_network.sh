eb4ec1dda392c444c7dd9e752be3485b5ce62f9e
Bruno <brunonahuelmarasi2@gmail.com>
2025-12-15 17:00:33 +0000

    Backup del servidor: 2025-12-15 17:00

--- iniciar_network.sh
+++ iniciar_network.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
-echo "--- INICIANDO LA NETWORK ---"
+echo "--- INICIANDO LA NETWORK (MODO 16GB) ---"
 
-# 1. Iniciar PROXY (Segundo plano)
+# 1. Iniciar PROXY (Velocity)
+# Asignamos 1GB. Es más que suficiente para miles de conexiones.
 echo "> Encendiendo Velocity (Proxy)..."
 cd Proxy
-java -Xms512M -Xmx512M -jar velocity.jar > log.txt 2>&1 &
+java -Xms1G -Xmx1G -jar velocity.jar > log.txt 2>&1 &
 PID_PROXY=$!
 cd ..
 
-# 2. Iniciar LOBBY (Segundo plano)
+# 2. Iniciar LOBBY
+# Asignamos 2GB. Ideal para que el login sea instantáneo.
 echo "> Encendiendo Lobby..."
 cd Lobby
-java -Xms512M -Xmx512M -jar paper.jar --nogui > log.txt 2>&1 &
+java -Xms2G -Xmx2G -jar paper.jar --nogui > log.txt 2>&1 &
 PID_LOBBY=$!
 cd ..
 
-# 3. Iniciar SURVIVAL (Primer plano)
+# 3. Iniciar SURVIVAL
+# Asignamos 10GB. ¡La bestia!
 echo "> Encendiendo Survival..."
 echo "> NOTA: Escribe 'stop' aquí para apagar todo la network."
 cd Survival
-java -Xms2G -Xmx2G -jar paper.jar --nogui 
+java -Xms10G -Xmx10G -jar paper.jar --nogui 
 
 # Apagado
 echo "--- APAGANDO EL RESTO DE LA NETWORK ---"
