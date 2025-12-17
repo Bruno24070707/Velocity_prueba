29f382732268443daa5928ed8c1fb273caa90d42
Bruno <brunonahuelmarasi2@gmail.com>
2025-12-17 21:49:51 +0000

    Backup del servidor: 2025-12-17 21:49

--- iniciar_network.sh
+++ iniciar_network.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 echo "--- INICIANDO LA NETWORK (MODO 16GB) ---"
 
+# Parámetro de Zona Horaria
+TIMEZONE="-Duser.timezone=America/Argentina/Buenos_Aires"
+
 # 1. Iniciar PROXY (Velocity)
-# Asignamos 1GB. Es más que suficiente para miles de conexiones.
 echo "> Encendiendo Velocity (Proxy)..."
 cd Proxy
-java -Xms1G -Xmx1G -jar velocity.jar > log.txt 2>&1 &
+java $TIMEZONE -Xms1G -Xmx1G -jar velocity.jar > log.txt 2>&1 &
 PID_PROXY=$!
 cd ..
 
 # 2. Iniciar LOBBY
-# Asignamos 2GB. Ideal para que el login sea instantáneo.
 echo "> Encendiendo Lobby..."
 cd Lobby
-java -Xms2G -Xmx2G -jar paper.jar --nogui > log.txt 2>&1 &
+java $TIMEZONE -Xms2G -Xmx2G -jar paper.jar --nogui > log.txt 2>&1 &
 PID_LOBBY=$!
 cd ..
 
 # 3. Iniciar SURVIVAL
-# Asignamos 10GB. ¡La bestia!
 echo "> Encendiendo Survival..."
-echo "> NOTA: Escribe 'stop' aquí para apagar todo la network."
+echo "> NOTA: Escribe 'stop' aquí para apagar toda la network."
 cd Survival
-java -Xms10G -Xmx10G -jar paper.jar --nogui 
+# Aquí aplicamos el parámetro a "la bestia"
+java $TIMEZONE -Xms10G -Xmx10G -jar paper.jar --nogui
 
 # Apagado
 echo "--- APAGANDO EL RESTO DE LA NETWORK ---"
